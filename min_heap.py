4a09cc967183064e6032ab1a6dfb44ec5047e66b
cahillbr <cahillbr@oregonstate.edu>
2023-03-03 18:05:50 -0800

    w

---
+++ min_heap.py
@@ -0,0 +1,253 @@
+# Name:
+# OSU Email:
+# Course: CS261 - Data Structures
+# Assignment:
+# Due Date:
+# Description:
+
+
+from dynamic_array import *
+
+
+class MinHeapException(Exception):
+    """
+    Custom exception to be used by MinHeap class
+    DO NOT CHANGE THIS CLASS IN ANY WAY
+    """
+    pass
+
+
+class MinHeap:
+    def __init__(self, start_heap=None):
+        """
+        Initialize a new MinHeap
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        self._heap = DynamicArray()
+
+        # populate MH with initial values (if provided)
+        # before using this feature, implement add() method
+        if start_heap:
+            for node in start_heap:
+                self.add(node)
+
+    def __str__(self) -> str:
+        """
+        Return MH content in human-readable form
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        heap_data = [self._heap[i] for i in range(self._heap.length())]
+        return 'HEAP ' + str(heap_data)
+
+    def add(self, node: object) -> None:
+        self._heap.append(node)
+        node_index = self._heap.length() - 1
+
+        while node_index > 0:
+            parent_index = (node_index - 1) // 2
+
+            if self._heap[parent_index] <= self._heap[node_index]:
+                break
+            else:
+                self._heap[parent_index], self._heap[node_index] = self._heap[node_index], self._heap[parent_index]
+                node_index = parent_index
+
+    def is_empty(self) -> bool:
+        """
+        Return True if the heap is empty; otherwise, it returns False.
+        The runtime complexity of this implementation is O(1).
+        """
+        return self._heap.length() == 0
+
+    def get_min(self) -> object:
+        """
+        Return the minimum key in the heap.
+        """
+        if self._heap.is_empty():
+            raise MinHeapException("MinHeap is empty")
+        return self._heap[0]
+
+    def remove_min(self) -> object:
+        """
+        This method returns an object with the minimum key and removes it from the heap. If the heap is empty, the
+        method raises a MinHeapException.
+
+        The runtime complexity of this implementation must be O(log N).
+        """
+        if self.is_empty():
+            raise MinHeapException("MinHeap is empty")
+
+        min_key = self._heap[0]
+        last_node = self._heap.pop()
+        self._heap[0] = last_node
+
+        parent_index = 0
+        while parent_index * 2 + 1 < self._heap.length():
+            left_child_index = parent_index * 2 + 1
+            right_child_index = parent_index * 2 + 2 if parent_index * 2 + 2 < self._heap.length() else left_child_index
+
+            smaller_child_index = left_child_index if self._heap[left_child_index] < self._heap[
+                right_child_index] else right_child_index
+            if self._heap[smaller_child_index] == self._heap[right_child_index] and self._heap[smaller_child_index] < \
+                    self._heap[parent_index]:
+                smaller_child_index = left_child_index
+
+            if self._heap[smaller_child_index] >= self._heap[parent_index]:
+                break
+
+            self._heap[parent_index], self._heap[smaller_child_index] = self._heap[smaller_child_index], self._heap[
+                parent_index]
+            parent_index = smaller_child_index
+
+        return min_key
+
+    def build_heap(self, da: DynamicArray) -> None:
+        """
+        This method receives a DynamicArray with objects in any order, and builds a proper MinHeap from them. The current content of the MinHeap is overwritten. The runtime complexity of this implementation must be O(N).
+        """
+        self._heap = da
+
+        for i in range(self._heap.length()//2, -1, -1):
+            self._min_heapify(i)
+
+    def _min_heapify(self, i: int) -> None:
+        """
+        This method takes an index of a node and makes a heap of the subtree rooted at this node.
+        """
+        smallest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < self._heap.length() and self._heap[left] < self._heap[smallest]:
+            smallest = left
+
+        if right < self._heap.length() and self._heap[right] < self._heap[smallest]:
+            smallest = right
+
+        if smallest != i:
+            self._heap[i], self._heap[smallest] = self._heap[smallest], self._heap[i]
+            self._min_heapify(smallest)
+
+    def size(self) -> int:
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def clear(self) -> None:
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+
+def heapsort(da: DynamicArray) -> None:
+    """
+    TODO: Write this implementation
+    """
+    pass
+
+
+# It's highly recommended that you implement the following optional          #
+# function for percolating elements down the MinHeap. You can call           #
+# this from inside the MinHeap class. You may edit the function definition.  #
+
+def _percolate_down(da: DynamicArray, parent: int) -> None:
+    """
+    TODO: Write your implementation
+    """
+    pass
+
+
+# ------------------- BASIC TESTING -----------------------------------------
+
+
+if __name__ == '__main__':
+
+    print("\nPDF - add example 1")
+    print("-------------------")
+    h = MinHeap()
+    print(h, h.is_empty())
+    for value in range(300, 200, -15):
+        h.add(value)
+        print(h)
+
+    print("\nPDF - add example 2")
+    print("-------------------")
+    h = MinHeap(['fish', 'bird'])
+    print(h)
+    for value in ['monkey', 'zebra', 'elephant', 'horse', 'bear']:
+        h.add(value)
+        print(h)
+
+    print("\nPDF - is_empty example 1")
+    print("-------------------")
+    h = MinHeap([2, 4, 12, 56, 8, 34, 67])
+    print(h.is_empty())
+
+    print("\nPDF - is_empty example 2")
+    print("-------------------")
+    h = MinHeap()
+    print(h.is_empty())
+
+    print("\nPDF - get_min example 1")
+    print("-----------------------")
+    h = MinHeap(['fish', 'bird'])
+    print(h)
+    print(h.get_min(), h.get_min())
+
+    print("\nPDF - remove_min example 1")
+    print("--------------------------")
+    h = MinHeap([1, 10, 2, 9, 3, 8, 4, 7, 5, 6])
+    while not h.is_empty() and h.is_empty() is not None:
+        print(h, end=' ')
+        print(h.remove_min())
+
+    print("\nPDF - build_heap example 1")
+    print("--------------------------")
+    da = DynamicArray([100, 20, 6, 200, 90, 150, 300])
+    h = MinHeap(['zebra', 'apple'])
+    print(h)
+    h.build_heap(da)
+    print(h)
+
+    print("--------------------------")
+    print("Inserting 500 into input DA:")
+    da[0] = 500
+    print(da)
+
+    print("Your MinHeap:")
+    print(h)
+    if h.get_min() == 500:
+        print("Error: input array and heap's underlying DA reference same object in memory")
+
+    print("\nPDF - heapsort example 1")
+    print("------------------------")
+    da = DynamicArray([100, 20, 6, 200, 90, 150, 300])
+    print(f"Before: {da}")
+    heapsort(da)
+    print(f"After:  {da}")
+
+    print("\nPDF - heapsort example 2")
+    print("------------------------")
+    da = DynamicArray(['monkey', 'zebra', 'elephant', 'horse', 'bear'])
+    print(f"Before: {da}")
+    heapsort(da)
+    print(f"After:  {da}")
+
+    print("\nPDF - size example 1")
+    print("--------------------")
+    h = MinHeap([100, 20, 6, 200, 90, 150, 300])
+    print(h.size())
+
+    print("\nPDF - size example 2")
+    print("--------------------")
+    h = MinHeap([])
+    print(h.size())
+
+    print("\nPDF - clear example 1")
+    print("---------------------")
+    h = MinHeap(['monkey', 'zebra', 'elephant', 'horse', 'bear'])
+    print(h)
+    print(h.clear())
+    print(h)
