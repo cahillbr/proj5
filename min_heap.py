4de82d6f8b866f50530cbe190f3a58952c8db403
cahillbr <cahillbr@oregonstate.edu>
2023-03-05 23:15:56 -0800

    w

--- min_heap.py
+++ min_heap.py
@@ -6,6 +6,7 @@
 # Description:
 
 
+
 from dynamic_array import *
 
 
@@ -69,63 +70,34 @@ class MinHeap:
 
     def remove_min(self) -> object:
         """
-        This method returns an object with the minimum key, and removes it from the heap.
-        If the heap is empty, the method raises a MinHeapException.
-
-        For the downward percolation of the replacement node:
-        if both children of the node have the same value (and are both smaller than the node),
-        swap with the left child.
+        This method returns an object with the minimum key and removes it from the heap. If the heap is empty, the
+        method raises a MinHeapException.
 
         The runtime complexity of this implementation must be O(log N).
         """
+        min_key = self._heap[0]
+
         if self.is_empty():
             raise MinHeapException("MinHeap is empty")
+        elif self._heap.length() == 1:
+            self._heap.pop()
+            return min_key
 
-        # Get the minimum value (at root) and replace with last leaf node
-        minimum = self._heap[0]
-        last_leaf_node = self._heap[self._heap.length() - 1]
-        self._heap[0] = last_leaf_node
-        self._heap.pop()
+        min_key = self._heap[0]
+        last_node = self._heap.pop()
+        self._heap[0] = last_node
 
-        # Perform a downward percolation to restore heap property
-        current_index = 0
-        while current_index < self._heap.length():
-            left_child_index = 2 * current_index + 1
-            right_child_index = 2 * current_index + 2
+        _percolate_down(self._heap, 0)
 
-            # If there is no child node, we are done.
-            if left_child_index >= self._heap.length():
-                break
-
-            # Determine the smallest child node
-            if right_child_index < self._heap.length():
-                left_child = self._heap[left_child_index]
-                right_child = self._heap[right_child_index]
-                if left_child <= right_child:
-                    smallest_child_index = left_child_index
-                else:
-                    smallest_child_index = right_child_index
-            else:
-                smallest_child_index = left_child_index
-
-            # Swap the current node with the smallest child node, if necessary
-            smallest_child = self._heap[smallest_child_index]
-            if last_leaf_node > smallest_child:
-                self._heap[current_index] = smallest_child
-                self._heap[smallest_child_index] = last_leaf_node
-                current_index = smallest_child_index
-            else:
-                break
-
-        return minimum
+        return min_key
 
     def build_heap(self, da: DynamicArray) -> None:
         """
         This method receives a DynamicArray with objects in any order, and builds a proper MinHeap from them. The current content of the MinHeap is overwritten. The runtime complexity of this implementation must be O(N).
         """
-        self._heap = da
+        self._heap = DynamicArray(list(da))
 
-        for i in range(self._heap.length()//2, -1, -1):
+        for i in range(self._heap.length() // 2, -1, -1):
             self._min_heapify(i)
 
     def _min_heapify(self, i: int) -> None:
@@ -155,9 +127,78 @@ class MinHeap:
 
 def heapsort(da: DynamicArray) -> None:
     """
-    TODO: Write this implementation
+    This function receives a DynamicArray and sorts its content
+    in non-ascending order, using the Heapsort algorithm.
     """
-    pass
+    n = da.length()
+
+    for i in range(n // 2 - 1, -1, -1):
+        parent = i
+        while parent * 2 + 1 < n:
+            child = parent * 2 + 1
+            if child + 1 < n and da[child] < da[child + 1]:
+                child += 1
+            if da[parent] < da[child]:
+                da[parent], da[child] = da[child], da[parent]
+                parent = child
+            else:
+                break
+
+    for i in range(n - 1, 0, -1):
+        da[0], da[i] = da[i], da[0]
+        parent = 0
+        while parent * 2 + 1 < i:
+            child = parent * 2 + 1
+            if child + 1 < i and da[child] < da[child + 1]:
+                child += 1
+            if da[parent] < da[child]:
+                da[parent], da[child] = da[child], da[parent]
+                parent = child
+            else:
+                break
+
+    start = 0
+    end = da.length() - 1
+
+    while start < end:
+        da[start], da[end] = da[end], da[start]
+        start += 1
+        end -= 1
+
+
+def heap_sort(da: DynamicArray) -> None:
+    """
+    This function receives a DynamicArray and sorts its content
+    in non-ascending order, using the Heapsort algorithm.
+    """
+    n = da.length()
+
+    # Build max heap
+    for i in range(n // 2 - 1, -1, -1):
+        parent = i
+        while parent * 2 + 1 < n:
+            child = parent * 2 + 1
+            if child + 1 < n and da[child] > da[child + 1]:
+                child += 1
+            if da[parent] < da[child]:
+                da[parent], da[child] = da[child], da[parent]
+                parent = child
+            else:
+                break
+
+    # Extract elements from heap one by one
+    for i in range(n - 1, 0, -1):
+        da[0], da[i] = da[i], da[0]
+        parent = 0
+        while parent * 2 + 1 < i:
+            child = parent * 2 + 1
+            if child + 1 < i and da[child] > da[child + 1]:
+                child += 1
+            if da[parent] < da[child]:
+                da[parent], da[child] = da[child], da[parent]
+                parent = child
+            else:
+                break
 
 
 # It's highly recommended that you implement the following optional          #
@@ -166,9 +207,18 @@ def heapsort(da: DynamicArray) -> None:
 
 def _percolate_down(da: DynamicArray, parent: int) -> None:
     """
-    TODO: Write your implementation
+     Swapping a node with its children until the node
+     is swapped into a position on the heap where it is already less
+     than both children
     """
-    pass
+    while (2 * parent + 1) < da.length():
+        smaller_idx = 2 * parent + 1
+        if (2 * parent + 2) < da.length() and da[2 * parent + 2] < da[2 * parent + 1]:
+            smaller_idx = 2 * parent + 2
+        if da[smaller_idx] > da[parent]:
+            return
+        da[smaller_idx], da[parent] = da[parent], da[smaller_idx]
+        parent = smaller_idx
 
 
 # ------------------- BASIC TESTING -----------------------------------------
